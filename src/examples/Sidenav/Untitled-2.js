059053e16ea2cc9aa271153ecb11ca7f102a7521
shaimatanni <tanni@devsstream.com>
2023-12-08 11:23:39 +0600

    first commit

---
+++ src/examples/Sidenav/Untitled-2.js
@@ -0,0 +1,189 @@
+//sumbenu work but not main menu
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { useLocation, NavLink } from "react-router-dom";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import Link from "@mui/material/Link";
+import Icon from "@mui/material/Icon";
+import Collapse from "@mui/material/Collapse";
+
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDButton from "components/MDButton";
+import SidenavCollapse from "examples/Sidenav/SidenavCollapse";
+import SidenavRoot from "examples/Sidenav/SidenavRoot";
+import sidenavLogoLabel from "examples/Sidenav/styles/sidenav";
+
+import {
+  useMaterialUIController,
+  setMiniSidenav,
+  setTransparentSidenav,
+  setWhiteSidenav,
+} from "context";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+function Sidenav({ color, brand, brandName, routes, ...rest }) {
+  const [controller, dispatch] = useMaterialUIController();
+  const { miniSidenav, transparentSidenav, whiteSidenav, darkMode, sidenavColor } = controller;
+  const location = useLocation();
+  const collapseName = location.pathname.replace("/", "");
+
+  let textColor = "white";
+
+  if (transparentSidenav || (whiteSidenav && !darkMode)) {
+    textColor = "dark";
+  } else if (whiteSidenav && darkMode) {
+    textColor = "inherit";
+  }
+
+  const closeSidenav = () => setMiniSidenav(dispatch, true);
+
+  const [openCollapseMap, setOpenCollapseMap] = useState({});
+
+  useEffect(() => {
+    function handleMiniSidenav() {
+      setMiniSidenav(dispatch, window.innerWidth < 1200);
+      setTransparentSidenav(dispatch, window.innerWidth < 1200 ? false : transparentSidenav);
+      setWhiteSidenav(dispatch, window.innerWidth < 1200 ? false : whiteSidenav);
+    }
+
+    window.addEventListener("resize", handleMiniSidenav);
+    handleMiniSidenav();
+
+    return () => window.removeEventListener("resize", handleMiniSidenav);
+  }, [dispatch, location, transparentSidenav, whiteSidenav]);
+
+  const handleCollapseToggle = (key) => {
+    setOpenCollapseMap((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
+
+  const renderRoutes = (routes || []).map(
+    ({ type, name, icon, title, noCollapse, key, href, route, collapse }) => {
+      let returnValue;
+
+      if (type === "collapse") {
+        returnValue = (
+          <React.Fragment key={key}>
+            <Link
+              href={href}
+              target="_blank"
+              rel="noreferrer"
+              sx={{ textDecoration: "none" }}
+              onClick={() => handleCollapseToggle(key)}
+            >
+              <SidenavCollapse
+                name={name}
+                icon={icon}
+                active={key === collapseName}
+                noCollapse={noCollapse}
+              />
+            </Link>
+            <Collapse in={openCollapseMap[key]} timeout="auto" unmountOnExit>
+              <List>
+                {Array.isArray(collapse) &&
+                  collapse.map((subRoute) => (
+                    <ListItem key={subRoute.key} button component={NavLink} to={subRoute.route}>
+                      <ListItemIcon>{subRoute.icon}</ListItemIcon>
+                      <ListItemText primary={subRoute.name} />
+                    </ListItem>
+                  ))}
+              </List>
+            </Collapse>
+          </React.Fragment>
+        );
+      } else if (type === "title") {
+        returnValue = (
+          <MDTypography
+            key={key}
+            color={textColor}
+            display="block"
+            variant="caption"
+            fontWeight="bold"
+            textTransform="uppercase"
+            pl={3}
+            mt={2}
+            mb={1}
+            ml={1}
+          >
+            {title}
+          </MDTypography>
+        );
+      } else if (type === "divider") {
+        returnValue = (
+          <Divider
+            key={key}
+            light={
+              (!darkMode && !whiteSidenav && !transparentSidenav) ||
+              (darkMode && !transparentSidenav && whiteSidenav)
+            }
+          />
+        );
+      }
+
+      return returnValue;
+    }
+  );
+
+  return (
+    <SidenavRoot
+      {...rest}
+      variant="permanent"
+      ownerState={{ transparentSidenav, whiteSidenav, miniSidenav, darkMode }}
+    >
+      <MDBox pt={3} pb={1} px={4} textAlign="center">
+        <MDBox
+          display={{ xs: "block", xl: "none" }}
+          position="absolute"
+          top={0}
+          right={0}
+          p={1.625}
+          onClick={closeSidenav}
+          sx={{ cursor: "pointer" }}
+        >
+          <MDTypography variant="h6" color="secondary">
+            <Icon sx={{ fontWeight: "bold" }}>close</Icon>
+          </MDTypography>
+        </MDBox>
+        <MDBox component={NavLink} to="/" display="flex" alignItems="center">
+          {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />}
+          <MDBox
+            width={!brandName && "100%"}
+            sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
+          >
+            <MDTypography component="h6" variant="button" fontWeight="medium" color={textColor}>
+              {brandName}
+            </MDTypography>
+          </MDBox>
+        </MDBox>
+      </MDBox>
+      <Divider
+        light={
+          (!darkMode && !whiteSidenav && !transparentSidenav) ||
+          (darkMode && !transparentSidenav && whiteSidenav)
+        }
+      />
+      <List>{renderRoutes}</List>
+      <MDBox p={2} mt="auto">
+        <MDButton variant="gradient" color={sidenavColor} fullWidth>
+          <NavLink to="authentication/sign-in">Log Out</NavLink>
+        </MDButton>
+      </MDBox>
+    </SidenavRoot>
+  );
+}
+
+Sidenav.defaultProps = {
+  color: "info",
+  brand: "",
+};
+
+Sidenav.propTypes = {
+  color: PropTypes.oneOf(["primary", "secondary", "info", "success", "warning", "error", "dark"]),
+  brand: PropTypes.string,
+  brandName: PropTypes.string.isRequired,
+  routes: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default Sidenav;
